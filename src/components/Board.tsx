d880720645f02d8e8ba68b849c3be5b7f4223114
Eduardo <eduardo@Eduardos-MacBook-Air.local>
2021-08-19 22:19:10 -0400

    useState separated into multiple variables

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -9,20 +9,14 @@ export default function Board(props: {
   onFinished: () => void;
 }) {
   const { onMakeMovement, onFinished } = props;
-  const [data, setData] = useState<{
-    cards: CardObject[];
-    isSelecting: boolean;
-    isComparing: boolean;
-  }>({
-    cards: [],
-    isSelecting: false,
-    isComparing: false,
-  });
+  const [cards, setCards] = useState<CardObject[]>([]);
+  const [isSelecting, setIsSelecting] = useState<boolean>(false);
+  const [isComparing, setIsComparing] = useState<boolean>(false);
 
   useEffect(() => {
     console.log("Set initial values");
-    setData({
-      cards: props.initialValues
+    setCards(
+      props.initialValues
         .concat(props.initialValues)
         .map((value) => ({
           ...value,
@@ -30,65 +24,61 @@ export default function Board(props: {
           isVisible: false,
           isSelected: false,
         }))
-        .sort(() => (Math.random() > 0.5 ? 1 : -1)),
-      isSelecting: false,
-      isComparing: false,
-    });
+        .sort(() => (Math.random() > 0.5 ? 1 : -1))
+    );
   }, [props.initialValues]);
 
   useEffect(() => {
-    if (data.isComparing) {
-      const selectedCards = data.cards.filter((card) => card.isSelected);
-      console.log("Comparing selected cards", selectedCards);
-      onMakeMovement();
-      setTimeout(() => {
-        if (selectedCards[0].groupId === selectedCards[1].groupId) {
-          console.log("Match");
-        } else {
-          console.log("Don't match");
-          selectedCards[0].isVisible = false;
-          selectedCards[1].isVisible = false;
-        }
-        setData({
-          cards: data.cards.map((card) => ({ ...card, isSelected: false })),
-          isSelecting: false,
-          isComparing: false,
-        });
-      }, 1200);
+    if (isComparing) {
+      const selectedCards = cards.filter((card) => card.isSelected);
+      if (selectedCards.length >= 2) {
+        console.log("Comparing selected cards", selectedCards);
+        onMakeMovement();
+        setTimeout(() => {
+          if (selectedCards[0].groupId === selectedCards[1].groupId) {
+            console.log("Match");
+          } else {
+            console.log("Don't match");
+            selectedCards[0].isVisible = false;
+            selectedCards[1].isVisible = false;
+          }
+          setCards(cards.map((card) => ({ ...card, isSelected: false })));
+          setIsSelecting(false);
+          setIsComparing(false);
+        }, 1200);
+      }
     }
 
-    if (!data.isSelecting) {
+    if (!isSelecting) {
       console.log("Checking if finished");
       if (
-        data.cards.length > 0 &&
-        data.cards.filter((card) => card.isVisible).length === data.cards.length
+        cards.length > 0 &&
+        cards.filter((card) => card.isVisible).length === cards.length
       ) {
         onFinished();
       }
     }
-  }, [data, onMakeMovement, onFinished]);
+  }, [isComparing, isSelecting, cards, onMakeMovement, onFinished]);
 
   function onSelectCardHandler(id: number) {
-    const cards = data.cards.map((card) =>
+    const updatedCards = cards.map((card) =>
       card.id === id ? { ...card, isVisible: true, isSelected: true } : card
     );
-    const selectedCards = cards.filter((card) => card.isSelected);
-    setData({
-      cards: cards,
-      isSelecting: true,
-      isComparing: selectedCards.length === 2,
-    });
+    const selectedCards = updatedCards.filter((card) => card.isSelected);
+    setCards(updatedCards);
+    setIsSelecting(true);
+    setIsComparing(selectedCards.length === 2);
   }
 
   console.log("Rendering");
 
   return (
     <div>
-      {data.cards.map((card: CardObject, i: number) => (
+      {cards.map((card: CardObject, i: number) => (
         <Card
           key={i}
           data={card}
-          onSelect={!data.isComparing ? onSelectCardHandler : () => {}}
+          onSelect={!isComparing ? onSelectCardHandler : () => {}}
         />
       ))}
     </div>
